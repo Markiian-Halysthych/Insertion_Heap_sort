6c141e5386848aa4330e420c2b80abb2ab5a189f
Markiian Halysthych <markijan8890.mg@gmail.com>
2020-12-13 12:10:02 +0200

    Solution for 2 lab

--- sort.py
+++ sort.py
@@ -1,9 +1,10 @@
 class Refrigerator:
-    def __init__(self, firm, capacity, weight, powerConsumption):
+    def __init__(self, firm, capacity, weight, power_consumption):
         self.firm = str(firm)
         self.capacity = float(capacity)
         self.weight = float(weight)
-        self.powerConsumption = float(powerConsumption)
+        self.power_consumption = float(power_consumption)
+
 
 refrigerator1 = Refrigerator("LG", 14.12, 250.0, 50.0)
 refrigerator2 = Refrigerator("Samsung", 15.13, 200.97, 28.16)
@@ -14,7 +15,7 @@ refrigerator6 = Refrigerator("LG", 30.86, 700.60, 75.98)
 refrigerator7 = Refrigerator("Samsung", 18.09, 10.60, 17.3)
 refrigerator8 = Refrigerator("Gorenja", 28.18, 600.50, 85.98)
 
-refrigertorCapacity = [
+refrigerator_capacity = [
     refrigerator1.capacity,
     refrigerator2.capacity,
     refrigerator3.capacity,
@@ -25,56 +26,58 @@ refrigertorCapacity = [
     refrigerator8.capacity
 ]
 
-refrigeratorPowerConsumption = [
-    refrigerator1.powerConsumption,
-    refrigerator2.powerConsumption,
-    refrigerator3.powerConsumption,
-    refrigerator4.powerConsumption,
-    refrigerator5.powerConsumption,
-    refrigerator6.powerConsumption,
-    refrigerator7.powerConsumption,
-    refrigerator8.powerConsumption
+refrigerator_power_consumption = [
+    refrigerator1.power_consumption,
+    refrigerator2.power_consumption,
+    refrigerator3.power_consumption,
+    refrigerator4.power_consumption,
+    refrigerator5.power_consumption,
+    refrigerator6.power_consumption,
+    refrigerator7.power_consumption,
+    refrigerator8.power_consumption
 ]
 
 
-def insertionSort(Arr):
-    for i in range(len(Arr)):
+def insertion_sort(arr):
+    for i in range(len(arr)):
         j = i - 1
-        key = Arr[i]
-        while Arr[j] < key and j >= 0:
-            Arr[j + 1] = Arr[j]
+        key = arr[i]
+        while arr[j] < key and j >= 0:
+            arr[j + 1] = arr[j]
             j -= 1
-        Arr[j + 1] = key
-    return Arr
+        arr[j + 1] = key
+    return arr
 
 
 def heapify(arr, n, i):
-	largest = i
-	left = 2 * i + 1
-	right = 2 * i + 2
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-	if left < n and arr[i] < arr[left]:
-		largest = left
-	if right < n and arr[largest] < arr[right]:
-		largest = right
-	if largest != i:
-		arr[i],arr[largest] = arr[largest],arr[i]
-		heapify(arr, n, largest)
+    if left < n and arr[i] < arr[left]:
+        largest = left
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]
+        heapify(arr, n, largest)
 
-def heapSort(arr):
-	n = len(arr)
-	for i in range(n // 2 - 1, -1, -1):
-		heapify(arr, n, i)
 
-	for i in range(n-1, 0, -1):
-		arr[i], arr[0] = arr[0], arr[i]
-		heapify(arr, i, 0)
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
 
-insertionSort(refrigertorCapacity)
-heapSort(refrigeratorPowerConsumption)
-print("Refrigerators sorted by capacity descending")
-print(refrigertorCapacity)
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+
+insertion_sort(refrigerator_capacity)
+heap_sort(refrigerator_power_consumption)
+print("Refrigerators sorted by capacity descending (insertion sort)")
+print(refrigerator_capacity)
 print()
-print("refrigerators sorted by power consumption")
-print(refrigeratorPowerConsumption)
+print("refrigerators sorted by power consumption (heap sort)")
+print(refrigerator_power_consumption)
 
